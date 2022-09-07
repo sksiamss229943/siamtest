46115a01949225c6ed6ba6426c5b2d6b048811d5
sksiamss229943@gmail.com <sksiamss229943@gmail.com>
2022-09-08 00:25:21 +0600

    Pikaco api with react

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import React from "react";
-import data from "./apis/data";
-import Card from "./components/Card";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(1);
+  const [name, setName] = useState();
+  const [moves, setMoves] = useState();
+  useEffect(() => {
+    async function getData() {
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+      setName(res.data.name)
+      setMoves(res.data.moves.length)
+    }
+    getData();
+  },[num]);
   return (
     <>
-      <div className="m-3">
-        <div className="row">
-          {data.map((e) => (
-            <>
-              <div className="col-3 my-3">
-                <Card data={e} />
-              </div>
-            </>
-          ))}
-        </div>
-      </div>
+      <h2>
+        You choose <span className="fw-bold">{num}</span> value
+      </h2>
+      <h2>My name is {name}</h2>
+      <h2>I have {moves} moves</h2>
+      <select value={num} onChange={(e) => setNum(e.target.value)}>
+        <option value="1">1</option>
+        <option value="25">
+          25
+        </option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
     </>
   );
 }
